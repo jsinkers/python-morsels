fa790d94b9ce2497d1a5d8496f15f2432bf4da91
James Sinclair <sinclair.j@gmail.com>
2019-10-08 17:30:51 +1100

    Suppress completed

--- suppress/suppress.py
+++ suppress/suppress.py
@@ -10,9 +10,10 @@ class suppress(ContextDecorator):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
-            print(f"exc_type: {exc_type}, exc_val: {exc_val}")
             suppress_exc = [issubclass(exc_type, err) for err in self.err]
             if True in suppress_exc:
-                return {"exception": exc_type, "traceback": exc_tb}
+                self.exception = exc_val
+                self.traceback = exc_tb
+                return True
 
         return False
