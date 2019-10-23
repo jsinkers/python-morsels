23ce8fa507c609da94ce3bb30ef7a125e1392202
James Sinclair <sinclair.j@gmail.com>
2019-10-23 14:55:49 +1100

    Some tests passed

---
+++ OrderedSet/orderedset.py
@@ -0,0 +1,39 @@
+class OrderedSet:
+    def __init__(self, items):
+        self.items = []
+        for item in items:
+            if item not in self.items:
+                self.items.append(item)
+
+        #self.items = items
+        self.set_items = set(items)
+
+    def __contains__(self, item):
+        return item in self.set_items
+
+    #@property
+    #def items(self):
+    #    return self._items
+
+    #@items.setter
+    #def items(self, item):
+    #    self._items.append(self.items)
+    #    self.set_items = set(self.items)
+    def __len__(self):
+        return len(self.items)
+
+
+    def add(self, item):
+        if item not in self.set_items:
+            self.items.append(item)
+            self.set_items.add(item)
+            #self.set_items()
+
+    def discard(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        return str(self.items)
