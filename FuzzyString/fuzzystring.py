209cef32a7ce075ea9aedf5513bc8b8a12fd659a
James Sinclair <sinclair.j@gmail.com>
2019-12-06 15:39:44 +1100

    FuzzyString: bonus 2 solved

--- FuzzyString/fuzzystring.py
+++ FuzzyString/fuzzystring.py
@@ -3,22 +3,33 @@ class FuzzyString:
         self.string = string
 
     def __eq__(self, other):
-        return self.string.lower() == other.lower()
+        return self.lstring == other.lower()
 
     def __le__(self, other):
-        return self.string.lower() <= other.lower()
+        return self.lstring <= other.lower()
 
     def __ge__(self, other):
-        return self.string.lower() >= other.lower()
+        return self.lstring >= other.lower()
 
     def __lt__(self, other):
-        return self.string.lower() < other.lower()
+        return self.lstring < other.lower()
 
     def __gt__(self, other):
-        return self.string.lower() > other.lower()
+        return self.lstring > other.lower()
 
     def __repr__(self):
         return repr(self.string)
 
     def __str__(self):
         return str(self.string)
+    
+    def __contains__(self, item):
+        return item.lower() in self.lstring
+
+    def __add__(self, item):
+        self.string += item
+        return self
+
+    @property
+    def lstring(self):
+        return self.string.lower()
